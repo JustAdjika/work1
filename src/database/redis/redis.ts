089f7afe64bb7071ad813e9ceadeff0a92bb9d4c
JustAdjika <fenixxfns@gmail.com>
2026-05-13 08:48:26 +0300

    refactor: redis config

--- src/database/redis/redis.ts
+++ src/database/redis/redis.ts
@@ -1,7 +1,13 @@
 import { createClient } from "redis";
 
+const url = process.env.REDIS_URL;
+
+if(!url) {
+    throw new Error('Missing DataBase environment variables');
+};
+
 const redis = createClient({
-  url: "redis://localhost:6379",
+  url,
 });
 
 redis.on("error", (err) => {
